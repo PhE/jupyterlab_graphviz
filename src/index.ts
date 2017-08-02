3c503f01addc9f8b0a9faf004059fd85c8e321a0
Philippe ENTZMANN <philippe@phec.net>
2017-08-02 10:56:50 +0200

    basic dot file viewer

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,101 @@
-import {
-  JupyterLabPlugin
-} from '@jupyterlab/application';
+/*-----------------------------------------------------------------------------
+| Copyright (c) Philippe Entzmann
+| Distributed under the terms of the Modified BSD License.
+|----------------------------------------------------------------------------*/
 
+import {
+  IRenderMime
+} from '@jupyterlab/rendermime-interfaces';
+
+import {
+  Widget
+} from '@phosphor/widgets';
 
 import * as Viz from "viz.js";
-import * as BpmnViewer from "bpmn-js";
 
-/**
- * Initialization data for the jupyterlab_myext extension.
- */
-const extension: JupyterLabPlugin<void> = {
-  id: 'jupyterlab_myext',
-  autoStart: true,
-  activate: (app) => {
-    console.log('JupyterLab extension jupyterlab_myext is activated!');
-    console.log('DBG 4');
-    //window["philippe"] = 'Hello !';
-    (<any>window).philippe = "Something";
-    (<any>window).Viz = Viz;
-    //var viewer = new BpmnViewer({ container: 'body' });
-    (<any>window).BpmnViewer = BpmnViewer;
+//TODO: fix the tsc path error
+//import "../style/index.css";
 
-    console.log('DBG 4b');
-    console.log(Viz);
-    console.log(Viz("digraph { a -> b; }"));
+// cf https://www.iana.org/assignments/media-types/text/vnd.graphviz
+const TYPES: {[key: string]: {name: string, extensions: string[], engine: any}} = {
+  'application/vnd.graphviz': {
+    name: 'dot',
+    extensions: ['.gv', '.dot'],
+    engine: 'dot'
   }
 };
 
-export default extension;
+/**
+ * A widget for rendering data, for usage with rendermime.
+ */
+export
+class RenderedData extends Widget implements IRenderMime.IRenderer {
+  /**
+   * Create a new widget for rendering Vega/Vega-Lite.
+   */
+  constructor(options: IRenderMime.IRendererOptions) {
+    super();
+    this._mimeType = options.mimeType;
+    this._engine = TYPES[this._mimeType].engine;
+    this.addClass('jp-graphviz');
+    this.div = document.createElement('div');
+    //(<any>window).Viz = Viz;
+
+    this.viz = Viz(`digraph { "loading ..."; }`);
+    this.div.innerHTML = this.viz;
+    //console.log(this.viz);
+
+
+    this.node.appendChild(this.div);
+  }
+
+  /**
+   * Render into this widget's node.
+   */
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    let data = model.data[this._mimeType];
+    //this._resetWidth();
+    this.viz = Viz(data);
+    //result = Viz("graph { n0 -- n1 -- n2 -- n3 -- n0; }", { engine: "neato" });
+    this.div.innerHTML = this.viz;
+    return Promise.resolve();
+  }
+
+  viz: any;
+  div: any;
+  private _mimeType: string;
+  private _engine: any;
+}
+
+/**
+ * A mime renderer factory for data.
+ */
+export
+const rendererFactory: IRenderMime.IRendererFactory = {
+  safe: false,
+  mimeTypes: Object.keys(TYPES),
+  createRenderer: options => new RenderedData(options)
+};
+
+const extensions = Object.keys(TYPES).map(k => {
+  const name = TYPES[k].name;
+  return {
+    name,
+    rendererFactory,
+    rank: 0,
+    dataType: 'string',
+    fileTypes: [{
+      name,
+      extensions: TYPES[k].extensions,
+      mimeTypes: [k]
+    }],
+    documentWidgetFactoryOptions: {
+      name,
+      primaryFileType: name,
+      fileTypes: [name],
+      defaultFor: [name],
+    }
+  } as IRenderMime.IExtension;
+});
+
+export default extensions;
